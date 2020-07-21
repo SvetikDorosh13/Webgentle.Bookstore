2944673ae6e25f2346cb5580f004ebbc672e86d0
Svetlana Doroshenko <sdoroshe@centralstatesfunds.org>
2020-07-21 11:18:50 -0500

    Added error messages to add new book, custom error messages and custom name

--- Webgentle.Bookstore/Models/BookModel.cs
+++ Webgentle.Bookstore/Models/BookModel.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Webgentle.Bookstore.Models
 {
     public class BookModel
     {
         public int Id { get; set; }
+        [StringLength(100, MinimumLength = 5)]
+        [Required(ErrorMessage ="Please enter the title of your book")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter the author name")]
         public string Author { get; set; }
+        [StringLength(500, MinimumLength = 30)]
         public string Description { get; set; }
         public string Category { get; set; }
         public string Language { get; set; }
-        public int TotalPages { get; set; }
+        [Required(ErrorMessage = "Please enter total pages")]
+        [Display(Name = "Total Pages")]
+        public int? TotalPages { get; set; }
     }
 }
